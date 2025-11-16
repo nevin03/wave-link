d42df25f103cdf38ec77cca963932cd9a53454ef
nevin03 <nevinm2021@gmail.com>
2025-11-17 00:34:34 +0530

    add : footer

--- src/app/counters/page.jsx
+++ src/app/counters/page.jsx
@@ -1,11 +1,13 @@
 import Counters from '@/modules/Counters/Counters';
 import CountersList from '@/modules/CountersList/CountersList';
+import Footer from '@/modules/Footer';
 
 export default function CountersPage() {
   return (
 <>
   <Counters />
   <CountersList/>
+  <Footer/>
 </>  
 
 
