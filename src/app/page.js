d615f425f6e5987c1e870de6dc64d84a744b68c3
nevin03 <nevinm2021@gmail.com>
2025-11-17 00:11:26 +0530

    add : pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import LandingPage from "@/modules/LandingPage/LandingPage";
 import ExpertServices from '@/modules/ExpertServices/index';
+import Footer from "@/modules/Footer";
+import ProductImage from "@/modules/ProductImage";
+import ServicesVideo from "@/modules/ExpertServices/ServicesVideo";
+import CounterLandingPage from "@/modules/Counters";
 
 export default function Home() {
   return (
     <>
       <LandingPage />
       <ExpertServices />
+      <ProductImage/>
+      <CounterLandingPage/>
+      <Footer/>
     </>
   );
 }
